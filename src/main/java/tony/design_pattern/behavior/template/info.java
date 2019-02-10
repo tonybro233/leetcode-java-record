8938c12186def0c9c7d52a09972ba4ed2a6aad3c
tony <x2040052@126.com>
2019-02-11 00:05:54 +0800

    template

---
+++ src/main/java/tony/design_pattern/behavior/template/info.java
@@ -0,0 +1,6 @@
+/**
+ * 一个抽象类公开定义了执行它的方法的方式/模板。它的子类可以按需要重写方法实现，
+ * 但调用将以抽象类中定义的方式进行。模板方法模式就是基于继承的代码复用技术，
+ * 在java语言层面的abstract和extend就是最基础的模板模式
+ */
+package tony.design_pattern.behavior.template;
