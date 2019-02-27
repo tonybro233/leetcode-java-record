fd95f3ce7542c491e8507366c620589daf857001
tony <x2040052@126.com>
2019-02-27 21:51:22 +0800

    update sort implementation

--- src/main/java/tony/alg/sort/BubbleSort.java
+++ src/main/java/tony/alg/sort/BubbleSort.java
@@ -13,10 +13,15 @@ public class BubbleSort<T extends Comparable<T>> extends SortBase<T> {
     public void sort(T[] a) {
         int N = a.length;
         for (int i = N-1 ; i > 0; i--){
+            boolean noChange = true;
             for (int j = 0; j < i; j++){
                 if (a[j].compareTo(a[j+1]) > 0){
                     exch(a,j,j+1);
+                    noChange = false;
                 }
+            }
+            if (noChange){
+                break;
             }
         }
     }
--- src/main/java/tony/alg/sort/InsertionSort.java
+++ src/main/java/tony/alg/sort/InsertionSort.java
@@ -12,9 +12,12 @@ public class InsertionSort<T extends Comparable<T>> extends SortBase<T> {
     @Override
     public void sort(T[] a) {
         for (int i = 1; i < a.length;i++){
-            for (int j = i; j > 0 && a[j].compareTo(a[j-1]) < 0;j--){
-                exch(a,j,j-1);
+            T tmp = a[i];
+            int j = i;
+            for ( ; j > 0 && a[j-1].compareTo(tmp) > 0;j--){
+                a[j] = a[j-1];
             }
+            a[j] = tmp;
         }
     }
 }
