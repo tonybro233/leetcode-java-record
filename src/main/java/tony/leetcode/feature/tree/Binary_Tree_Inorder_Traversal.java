a8b194a052aec00b8c130442b51e89670b5a8fcf
tony <x2040052@126.com>
2019-03-30 18:31:25 +0800

    update binary tree traversal

--- src/main/java/tony/leetcode/feature/tree/Binary_Tree_Inorder_Traversal.java
+++ src/main/java/tony/leetcode/feature/tree/Binary_Tree_Inorder_Traversal.java
@@ -7,24 +7,24 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
-// 144. 二叉树的前序遍历
-// 给定一个二叉树，返回它的 前序 遍历。
+// 94. 二叉树的中序遍历
+// 给定一个二叉树，返回它的中序 遍历。
 //
-//  示例:
+// 示例:
 // 输入: [1,null,2,3]
 //    1
 //     \
 //      2
 //     /
 //    3
-//
-// 输出: [1,2,3]
+// 输出: [1,3,2]
+
 // 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
 
-public class Binary_Tree_Preorder_Traversal {
+public class Binary_Tree_Inorder_Traversal {
 
     // 非递归
-    public List<Integer> preorderTraversal2(TreeNode root) {
+    public List<Integer> inorderTraversal2(TreeNode root) {
         Deque<TreeNode> stack = new LinkedList<>();
         List<Integer> result = new ArrayList<>();
         if (null == root){
@@ -32,11 +32,11 @@ public class Binary_Tree_Preorder_Traversal {
         }
         TreeNode last = null;
         stack.addLast(root);
-        result.add(root.val);
         while (null != stack.peekLast()){
             TreeNode node = stack.peekLast();
             if (null == node.left && null == node.right){
                 // 叶子节点
+                result.add(node.val);
                 last = stack.pollLast();
                 continue;
             }
@@ -47,18 +47,17 @@ public class Binary_Tree_Preorder_Traversal {
             }
             if (null == node.left){
                 // 只有右节点
+                result.add(node.val);
                 stack.addLast(node.right);
-                result.add(node.right.val);
             } else {
                 if (last != node.left){
                     // 左节点入队
                     stack.addLast(node.left);
-                    result.add(node.left.val);
                 } else {
                     // 左节点返回
+                    result.add(node.val);
                     if (null != node.right){
                         stack.addLast(node.right);
-                        result.add(node.right.val);
                     } else {
                         last = stack.pollLast();
                     }
@@ -69,7 +68,7 @@ public class Binary_Tree_Preorder_Traversal {
     }
 
     // 递归
-    public List<Integer> preorderTraversal(TreeNode root) {
+    public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         go(root, result);
         return result;
@@ -79,18 +78,8 @@ public class Binary_Tree_Preorder_Traversal {
         if (null == node){
             return;
         }
-        result.add(node.val);
         go(node.left, result);
+        result.add(node.val);
         go(node.right, result);
-    }
-
-    public static void main(String[] args){
-        TreeNode n1 = new TreeNode(1);
-        TreeNode n2 = new TreeNode(2);
-        TreeNode n3 = new TreeNode(3);
-        n1.right = n2;
-        n2.left = n3;
-        List<Integer> list = new Binary_Tree_Preorder_Traversal().preorderTraversal2(n1);
-        System.out.println(list);
     }
 }
--- src/main/java/tony/leetcode/feature/tree/Binary_Tree_Postorder_Traversal.java
+++ src/main/java/tony/leetcode/feature/tree/Binary_Tree_Postorder_Traversal.java
@@ -7,24 +7,25 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
-// 144. 二叉树的前序遍历
-// 给定一个二叉树，返回它的 前序 遍历。
+// 145. 二叉树的后序遍历
+// 给定一个二叉树，返回它的 后序 遍历。
 //
-//  示例:
+// 示例:
 // 输入: [1,null,2,3]
 //    1
 //     \
 //      2
 //     /
 //    3
-//
-// 输出: [1,2,3]
+// 输出: [3,2,1]
+
 // 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
 
-public class Binary_Tree_Preorder_Traversal {
+public class Binary_Tree_Postorder_Traversal {
+
 
     // 非递归
-    public List<Integer> preorderTraversal2(TreeNode root) {
+    public List<Integer> postorderTraversal2(TreeNode root) {
         Deque<TreeNode> stack = new LinkedList<>();
         List<Integer> result = new ArrayList<>();
         if (null == root){
@@ -32,7 +33,6 @@ public class Binary_Tree_Preorder_Traversal {
         }
         TreeNode last = null;
         stack.addLast(root);
-        result.add(root.val);
         while (null != stack.peekLast()){
             TreeNode node = stack.peekLast();
             if (null == node.left && null == node.right){
@@ -42,34 +42,48 @@ public class Binary_Tree_Preorder_Traversal {
             }
             if (null != node.right && last == node.right){
                 // 右节点返回
+                result.add(last.val);
                 last = stack.pollLast();
                 continue;
             }
             if (null == node.left){
                 // 只有右节点
                 stack.addLast(node.right);
-                result.add(node.right.val);
             } else {
                 if (last != node.left){
                     // 左节点入队
                     stack.addLast(node.left);
-                    result.add(node.left.val);
                 } else {
                     // 左节点返回
+                    result.add(last.val);
                     if (null != node.right){
                         stack.addLast(node.right);
-                        result.add(node.right.val);
                     } else {
                         last = stack.pollLast();
                     }
                 }
             }
         }
+        result.add(last.val);
+
         return result;
     }
 
+    public static void main(String[] args){
+        List<Integer> list = new Binary_Tree_Postorder_Traversal().postorderTraversal2(new TreeNode(1));
+        System.out.println(list);
+
+        // TreeNode n1 = new TreeNode(1);
+        // TreeNode n2 = new TreeNode(2);
+        // TreeNode n3 = new TreeNode(3);
+        // n3.left = n1;
+        // n1.right = n2;
+        // List<Integer> list = new Binary_Tree_Postorder_Traversal().postorderTraversal2(n3);
+        // System.out.println(list);
+    }
+
     // 递归
-    public List<Integer> preorderTraversal(TreeNode root) {
+    public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         go(root, result);
         return result;
@@ -79,18 +93,8 @@ public class Binary_Tree_Preorder_Traversal {
         if (null == node){
             return;
         }
-        result.add(node.val);
         go(node.left, result);
         go(node.right, result);
-    }
-
-    public static void main(String[] args){
-        TreeNode n1 = new TreeNode(1);
-        TreeNode n2 = new TreeNode(2);
-        TreeNode n3 = new TreeNode(3);
-        n1.right = n2;
-        n2.left = n3;
-        List<Integer> list = new Binary_Tree_Preorder_Traversal().preorderTraversal2(n1);
-        System.out.println(list);
+        result.add(node.val);
     }
 }
