7b750178f57bf8a68ad444f8f7a3d5c6a5df4e41
tony <x2040052@126.com>
2019-06-29 23:37:09 +0800

    594 623

---
+++ src/main/java/tony/leetcode/feature/greedy/Longest_Harmonious_Subsequence.java
@@ -0,0 +1,31 @@
+package tony.leetcode.feature.greedy;
+
+import java.util.Arrays;
+
+// 594. 最长和谐子序列
+// 和谐数组是指一个数组里元素的最大值和最小值之间的差别正好是1。
+// 现在，给定一个整数数组，你需要在所有可能的子序列中找到最长的和谐子序列的长度。
+//
+// 示例 1:
+// 输入: [1,3,2,2,5,2,3,7]
+// 输出: 5
+// 原因: 最长的和谐数组是：[3,2,2,2,3].
+
+// 说明: 输入的数组长度最大不超过20,000.
+
+public class Longest_Harmonious_Subsequence {
+
+    public int findLHS(int[] nums) {
+        Arrays.sort(nums);
+        int begin = 0,res = 0;
+        for(int end = 0;end < nums.length;end++){
+            while(nums[end] - nums[begin] > 1) {
+                begin++;
+            }
+            if (nums[end] - nums[begin] == 1) {
+                res = Math.max(res,end - begin + 1);
+            }
+        }
+        return res;
+    }
+}
---
+++ src/main/java/tony/leetcode/feature/tree/Add_One_Row_to_Tree.java
@@ -0,0 +1,99 @@
+package tony.leetcode.feature.tree;
+
+import tony.util.TreeNode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+// 623. 在二叉树中增加一行
+// 给定一个二叉树，根节点为第1层，深度为 1。在其第 d 层追加一行值为 v 的节点。
+// 添加规则：给定一个深度值 d （正整数），针对深度为 d-1 层的每一非空节点 N，
+// 为 N 创建两个值为 v 的左子树和右子树。
+// 将 N 原先的左子树，连接为新节点 v 的左子树；将 N 原先的右子树，连接为新节点 v 的右子树。
+// 如果 d 的值为 1，深度 d - 1 不存在，则创建一个新的根节点 v，原先的整棵树将作为 v 的左子树。
+//
+// 示例 1:
+// 输入:
+// 二叉树如下所示:
+//        4
+//      /   \
+//     2     6
+//    / \   /
+//   3   1 5
+//
+// v = 1
+// d = 2
+//
+// 输出:
+//        4
+//       / \
+//      1   1
+//     /     \
+//    2       6
+//   / \     /
+//  3   1   5
+//
+// 示例 2:
+// 输入:
+// 二叉树如下所示:
+//       4
+//      /
+//     2
+//    / \
+//   3   1
+//
+// v = 1
+// d = 3
+//
+// 输出:
+//       4
+//      /
+//     2
+//    / \
+//   1   1
+//  /     \
+// 3       1
+
+// 注意:
+// 输入的深度值 d 的范围是：[1，二叉树最大深度 + 1]。
+// 输入的二叉树至少有一个节点。
+
+public class Add_One_Row_to_Tree {
+
+    // 常规bfs
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if (1 == d){
+            TreeNode newRoot = new TreeNode(v);
+            newRoot.left = root;
+            return newRoot;
+        }
+
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.addLast(root);
+
+        while (d > 2){
+            int size = deque.size();
+            for (int i = 0;i < size;i++){
+                TreeNode node = deque.pollFirst();
+                if (null != node.left){
+                    deque.addLast(node.left);
+                }
+                if (null != node.right){
+                    deque.addLast(node.right);
+                }
+            }
+            d--;
+        }
+
+        while (null != deque.peekFirst()){
+            TreeNode node = deque.pollFirst();
+            TreeNode newLeft = new TreeNode(v), newRight = new TreeNode(v);
+            newLeft.left = node.left;
+            newRight.right = node.right;
+            node.left = newLeft;
+            node.right = newRight;
+        }
+
+        return root;
+    }
+}
