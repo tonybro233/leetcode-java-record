88fd41e33934ce8ad1bede28217a7afd55977e6e
tony <x2040052@126.com>
2022-12-13 21:49:08 +0800

    调整二分说明

--- src/main/java/tony/alg/BasicBinary.java
+++ src/main/java/tony/alg/BasicBinary.java
@@ -10,16 +10,14 @@ public class BasicBinary {
         // while循环的表达式表达了二分搜索的区间
         // 如果采用 <=
         //   终止条件为 low = high + 1
-        //   所以终止之前的计算会出现 low = high 的情况，此时 mid = low = high
-        //   所以 high 的初始值要取为 len - 1
-        //   所以搜索区间(mid所在区间)为 [low, high]
+        //   终止之前的计算会出现 low = high 的情况，临界时 mid = low = high
+        //   所以搜索区间(mid所在区间)为 [low, high], 所以 high 的初始值要取为 len - 1
         //   所以进行下一轮时的操作是 low = mid + 1 和 high = mid - 1
 
         // 如果采用 <
         //   终止条件为 low = high
-        //   终止之前的计算至多只会出现 low = high - 1，此时 mid = low = high - 1
-        //   所以 high 的初始值要取为 len
-        //   所以搜索区间(mid所在区间)为 [low, high)
+        //   终止之前的计算至多只会出现 low = high - 1，临界时 mid = low = high - 1
+        //   所以搜索区间(mid所在区间)为 [low, high), 所以 high 的初始值要取为 len
         //   所以进行下一轮时的操作是 low = mid + 1 和 high = mid
 
 
@@ -43,7 +41,8 @@ public class BasicBinary {
             }
         }
 
-        System.out.printf("大于等于目标最小:%d 小于目标最大:%d%n", l1, h1);
+        System.out.printf("大于等于目标最小:%s(%s) 小于目标最大:%s(%s)%n",
+                arrayVal(array, l1), l1, arrayVal(array, h1), h1);
 
         int l2 = 0;
         int h2 = array.length - 1;
@@ -63,7 +62,8 @@ public class BasicBinary {
             }
         }
 
-        System.out.printf("大于目标最小:%d 小于等于目标最大:%d%n", l2, h2);
+        System.out.printf("大于目标最小:%s(%s) 小于等于目标最大:%s(%s)%n",
+                arrayVal(array, l2), l2, arrayVal(array, h2), h2);
 
         int l3 = 0;
         int h3 = array.length - 1; // 注意这里取的是 len
@@ -83,7 +83,8 @@ public class BasicBinary {
 
         // l3 == h3
         // 找不到元素的时候取的是 大于目标最小
-        System.out.printf("左边界1:%d low=%d high=%d%n", l3, l3, h3);
+        System.out.printf("左边界(大于等于目标最小):%s(%s) low=%s(%s) high=%s(%s)%n",
+                arrayVal(array, l3), l3, arrayVal(array, l3), l3, arrayVal(array, h3), h3);
 
         int l4 = 0;
         int h4 = array.length - 1;
@@ -101,7 +102,8 @@ public class BasicBinary {
         // l4 == h4
         // 注意 -1
         // 找不到元素时取的是 小于目标最大
-        System.out.printf("右边界:%d low=%d high=%d%n", l4 - 1, l4, h4);
+        System.out.printf("右边界(小于等于目标最大):%s(%s) low=%s(%s) high=%s(%s)%n",
+                arrayVal(array, l4 - 1), l4 - 1, arrayVal(array, l4), l4, arrayVal(array, h4), h4);
 
 
 
@@ -120,7 +122,17 @@ public class BasicBinary {
         }
         // l5 = h5 + 1
         // 这里其实就写的和第一个例子一模一样了
-        System.out.printf("左边界:%d low=%d high=%d%n", l5, l5, h5);
+        System.out.printf("左边界():%s(%s) low=%s(%s) high=%s(%s)%n", arrayVal(array, l5), l5, arrayVal(array, l5), l5, arrayVal(array, l5), h5);
+    }
+
+    private static String arrayVal(int[] array, int idx) {
+        if (idx < 0) {
+            return "L";
+        } else if (idx >= array.length) {
+            return "R";
+        } else {
+            return Integer.toString(array[idx]);
+        }
     }
 
     public static void searchTest(int[] nums, int target) {
@@ -142,13 +154,12 @@ public class BasicBinary {
     }
 
     public static void main(String[] args) {
-        int[] array = new int[]{1, 2, 3, 4, 6, 6, 8, 9};
-        int target = 6;
+        int[] array = new int[]{1, 2, 3, 4, 6, 6, 8, 10};
+        int target = 9;
         // System.out.println("数组=" + Arrays.toString(array) + " 目标=" + target);
         // search(array, target);
 
-        target = 4;
-        System.out.println("数组=" + Arrays.toString(array) + " 目标=" + target);
+        System.out.println("数组=" + Arrays.toString(array) + " 长度=" + array.length + " 目标=" + target);
         search(array, target);
 
         // searchTest(array, 9);
